20a59a77cd17c40f11c55f9121b92f5954e7ea91
Your Name <sumampouw.2912@gmail.com>
2024-09-23 18:09:33 +0800

    second commit

--- js/js.js
+++ js/js.js
@@ -1,1 +1,12 @@
-// js js
+// Add this to your JavaScript file
+const slideshow = document.querySelector('.slideshow');
+const images = slideshow.querySelectorAll('img');
+let currentImageIndex = 0;
+
+function nextImage() {
+  images[currentImageIndex].classList.remove('active');
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  images[currentImageIndex].classList.add('active');
+}
+
+setInterval(nextImage, 3000); // change the interval time as needed
